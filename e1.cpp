610f96afc82fb4c820f1ccef94060d816feb542c
Jani <janij.timonen@gmail.com>
2016-01-21 10:46:12 +0200

    merge

--- e1.cpp
+++ e1.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 
-int main() {
-	cout << "Hello stranger. What is your name?\n";
-	cout << "Enter your name: ";
-	
-	char name[50];
-	cin >> name;
-	cout << "\nHello!" << name;
-	
-	return 0;
+int main()
+{
+    cout << "Hello stranger. What is your name?\n";
+    cout << "Enter your name: ";
+
+    char name[50];
+    cin >> name;
+    cout << "\nHello! " << name;
+
+    return 0;
 }
